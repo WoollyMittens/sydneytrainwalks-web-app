19422b7a032f96ec866d9f8c2e8d95388e15f680
Maurice van Creij <maurice.vancreij@webqem.com>
2018-05-07 13:13:45 +1000

    Add cordova

---
+++ cordova/addplugins.sh
@@ -0,0 +1,5 @@
+cordova plugin add cordova-plugin-console
+cordova plugin add cordova-plugin-geolocation
+cordova plugin add cordova-plugin-inappbrowser
+cordova plugin add cordova-plugin-whitelist
+cordova plugin add cordova-plugin-statusbar
---
+++ cordova/remplugins.sh
@@ -0,0 +1,5 @@
+cordova plugin remove cordova-plugin-console
+cordova plugin remove cordova-plugin-geolocation
+cordova plugin remove cordova-plugin-inappbrowser
+cordova plugin remove cordova-plugin-whitelist
+cordova plugin remove cordova-plugin-statusbar
