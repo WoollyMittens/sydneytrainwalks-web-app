9266532906db7634822eeefc1986bfd33cb54004
Maurice van Creij <maurice.vancreij@webqem.com>
2018-05-14 10:06:01 +1000

    Support android back button

--- inc/js/scripts.js
+++ inc/js/scripts.js
@@ -18578,6 +18578,7 @@ SydneyTrainWalks.prototype.Footer = function(parent) {
 	this.parent = parent;
 	this.config = parent.config;
 	this.config.extend({
+		'origin': 'menu',
 		'footer': document.querySelector('.toolbar'),
 		'footerTemplate': document.getElementById('footer-template')
 	});
@@ -18589,6 +18590,8 @@ SydneyTrainWalks.prototype.Footer = function(parent) {
 		this.update(null);
 		// add a global click handler to the footer
 		this.config.footer.addEventListener('click', this.onFooterClicked.bind(this));
+		// add the event handler for the browser back button
+		document.addEventListener("backbutton", this.onBackButton.bind(this));
 		// return the object
 		return this;
 	};
@@ -18600,18 +18603,38 @@ SydneyTrainWalks.prototype.Footer = function(parent) {
 
 	// EVENTS
 
+	this.onBackButton = function(evt) {
+		// if this is not an entry page
+		console.log("onBackButton", document.body.className);
+		if (!/menu|overview/.test(document.body.className)) {
+			// cancel the back button
+			evt.preventDefault();
+			// return to the origin page
+			window.localStorage.removeItem('id');
+			window.localStorage.removeItem('mode');
+			document.body.className = 'screen-' + this.config.origin;
+		// if this is a cordova app
+		} else if (navigator.app && navigator.app.exitApp) {
+			// close the app
+			navigator.app.exitApp();
+		}
+	}
+
 	this.onFooterClicked = function(evt) {
-		// cancel any clicks
-		evt.preventDefault();
 		// get the target of the click
 		var target = evt.target || evt.srcElement,
 			id = target.getAttribute('id');
 		// if a button was clicked
 		if (id && id.match(/footer-to-/)) {
-			// reset the local storage when returning to the menu
-			if (id.match(/-menu|-about/)) {
+			// cancel any clicks
+			evt.preventDefault();
+			// if this is a menu page
+			if (id.match(/-menu|-overview|-about/)) {
+				// reset the local storage when returning to the menu
 				window.localStorage.removeItem('id');
 				window.localStorage.removeItem('mode');
+				// remember what menu screen was the origin
+				this.config.origin = id.substr(10);
 			}
 			// apply the mode to the body
 			document.body.className = 'screen-' + id.substr(10);
