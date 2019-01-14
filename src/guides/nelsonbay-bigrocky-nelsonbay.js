f43fa7b84013c7671ddb8d20bfc868085b59c9a4
Maurice van Creij <maurice.vancreij@webqem.com>
2019-01-14 14:10:08 +1100

    Extend Nelson Bay walk

---
+++ src/guides/nelsonbay-bigrocky-nelsonbay.js
@@ -0,0 +1,57 @@
+{
+	"location" : "Big Rocky",
+	"description" : [
+		"This walk covers part of <a href=\"http://www.nationalparks.nsw.gov.au/tomaree-national-park\">Tomaree National Park</a>.",
+		"Be mindful that the bus back to Newcastle leaves very infrequently. Make sure you don't get stranded in Nelson Bay without transport."
+	],
+	"duration" : 8,
+	"length" : 28,
+	"zoom" : 13,
+	"rain" : false,
+	"fireban" : false,
+	"markers" : {
+		"start" : {
+			"location" : "Nelson Bay",
+			"type" : "bus",
+			"icon" : "./inc/img/marker-bus.png",
+			"description" : "Get the train and bus to Nelson Bay using <a href=\"https://transportnsw.info/trip#/?to=Nelson Bay\">transportnsw.info</a>."
+		},
+		"end" : {
+			"location" : "Nelson Bay",
+			"type" : "bus",
+			"icon" : "./inc/img/marker-bus.png",
+			"description" : "Plan your return trip from Nelson Bay at <a href=\"https://transportnsw.info/trip#/?from=Nelson Bay\">transportnsw.info</a>."
+		},
+		"kioska" : {
+			"type" : "kiosk",
+			"icon" : "./inc/img/marker-kiosk.png",
+			"lat" : -32.74295277777778,
+			"lon" : 152.1709972222222
+		},
+		"toileta" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -32.720502,
+			"lon" : 152.144634
+		},
+		"toiletb" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -32.712967,
+			"lon" : 152.158516
+		},
+		"toiletc" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -32.742580,
+			"lon" : 152.170929
+		}
+	},
+	"landmarks" : {
+		"dsc_1595" : ""
+	},
+	"indicator" : {
+		"icon" : "./inc/img/marker-photo.png",
+		"description" : "Photo taken at this location."
+	}
+}
