83c38ceeff2f3636ebcb32adf9542762546007f7
Maurice van Creij <maurice.vancreij@webqem.com>
2019-02-18 15:36:04 +1100

    Add Picton rail trail

---
+++ src/guides/mittagong-pictonloop-tahmoor.js
@@ -0,0 +1,57 @@
+{
+	"location" : "Picton Loop",
+	"description" : [
+		"This walk follows the <a href=\"http://www.railtrailsnsw.com.au/wp-content/uploads/2016/05/MittagongPictonRT_brochure.pdf\">proposed</a> <a href=\"https://en.wikipedia.org/wiki/Picton%E2%80%93Mittagong_loop_railway_line\">Picton-Mittagong rail trail</a>.",
+		"Thanks to Kynan Batt for his local knowledge in clarifying this route."
+	],
+	"duration" : 8,
+	"length" : 40,
+	"zoom" : 13,
+	"rain" : true,
+	"fireban" : true,
+	"markers" : {
+		"start" : {
+			"location" : "Mittagong",
+			"type" : "train",
+			"icon" : "./inc/img/marker-train.png",
+			"description" : "Get the train to Mittagong using <a href=\"https://transportnsw.info/trip#/?to=Mittagong\">transportnsw.info</a>."
+		},
+		"end" : {
+			"location" : "Tahmoor",
+			"type" : "train",
+			"icon" : "./inc/img/marker-train.png",
+			"description" : "Plan your return trip from Tahmoor at <a href=\"https://transportnsw.info/trip#/?from=Tahmoor\">transportnsw.info</a>."
+		},
+		"kioska" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-kiosk.png",
+			"lat": -34.35313598998312,
+			"lon": 150.4939696999867
+		},
+		"kioskb" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-kiosk.png",
+			"lat": -34.254959,
+			"lon": 150.534824
+		},
+		"toileta" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat": -34.451200350001145,
+			"lon": 150.4489249500143
+		},
+		"toiletb" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat": -34.257502979991486,
+			"lon": 150.53247977989923
+		}
+	},
+	"indicator" : {
+		"icon" : "./inc/img/marker-photo.png",
+		"description" : "Photo taken at this location."
+	},
+	"landmarks" : {
+		"dsc_2893" : ""
+	}
+}
