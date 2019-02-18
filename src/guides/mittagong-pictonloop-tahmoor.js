f52420100b6272587e40a88b6cc6236132109464
Maurice van Creij <maurice.vancreij@webqem.com>
2019-02-18 16:33:56 +1100

    Add Picton loop landmarks

--- src/guides/mittagong-pictonloop-tahmoor.js
+++ src/guides/mittagong-pictonloop-tahmoor.js
@@ -1,5 +1,5 @@
 {
-	"location" : "Picton Loop",
+	"location" : "Picton-Mittagong Loop",
 	"description" : [
 		"This walk follows the <a href=\"http://www.railtrailsnsw.com.au/wp-content/uploads/2016/05/MittagongPictonRT_brochure.pdf\">proposed</a> <a href=\"https://en.wikipedia.org/wiki/Picton%E2%80%93Mittagong_loop_railway_line\">Picton-Mittagong rail trail</a>.",
 		"Thanks to Kynan Batt for his local knowledge in clarifying this route."
@@ -23,13 +23,13 @@
 			"description" : "Plan your return trip from Tahmoor at <a href=\"https://transportnsw.info/trip#/?from=Tahmoor\">transportnsw.info</a>."
 		},
 		"kioska" : {
-			"type" : "toilet",
+			"type" : "Take away",
 			"icon" : "./inc/img/marker-kiosk.png",
 			"lat": -34.35313598998312,
 			"lon": 150.4939696999867
 		},
 		"kioskb" : {
-			"type" : "toilet",
+			"type" : "General store",
 			"icon" : "./inc/img/marker-kiosk.png",
 			"lat": -34.254959,
 			"lon": 150.534824
@@ -52,6 +52,50 @@
 		"description" : "Photo taken at this location."
 	},
 	"landmarks" : {
-		"dsc_2893" : ""
+		"dsc_2893" : "",
+		"dsc_2895" : "",
+		"dsc_2899" : "",
+		"dsc_2911" : "",
+		"dsc_2913" : "",
+		"dsc_2918" : "",
+		"dsc_2920" : "",
+		"dsc_2922" : "",
+		"dsc_2924" : "",
+		"dsc_2927" : "",
+		"dsc_2928" : "",
+		"dsc_2931" : "",
+		"dsc_2945" : "",
+		"dsc_2946" : "",
+		"dsc_2947" : "",
+		"dsc_2948" : "",
+		"dsc_2949" : "",
+		"dsc_2950" : "",
+		"dsc_2952" : "",
+		"dsc_2964" : "",
+		"dsc_2966" : "",
+		"dsc_2967" : "",
+		"dsc_2977" : "",
+		"dsc_2979" : "",
+		"dsc_2982" : "",
+		"dsc_3004" : "",
+		"dsc_3007" : "",
+		"dsc_3008" : "",
+		"dsc_3010" : "",
+		"dsc_3030" : "",
+		"dsc_3033" : "",
+		"dsc_3034" : "",
+		"dsc_3039" : "",
+		"dsc_3057" : "",
+		"dsc_3075" : "",
+		"dsc_3083" : "",
+		"dsc_3084" : "",
+		"dsc_3086" : "",
+		"dsc_3102" : "",
+		"dsc_3108" : "",
+		"dsc_3121" : "",
+		"dsc_3122" : "",
+		"dsc_3123" : "",
+		"dsc_3124" : "",
+		"dsc_3126" : ""
 	}
 }
