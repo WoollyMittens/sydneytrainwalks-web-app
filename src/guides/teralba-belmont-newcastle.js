8ab81898c417581eacdd64f5f81accb8826c0582
Maurice van Creij <maurice.vancreij@webqem.com>
2019-02-28 09:54:43 +1100

    Add teralba-belmont-newcastle

---
+++ src/guides/teralba-belmont-newcastle.js
@@ -0,0 +1,120 @@
+{
+	"location" : "Lake Macquarie",
+	"description" : [
+		"This walk passes through <a href=\"http://www.visitlakemac.com.au/valentine/green-point-reserve\">Green Point Reserve</a> along the shore of Lake Macquarie.",
+		"From Belmont this walk follows the <a href=\"http://www.railtrails.org.au/trail?view=trail&id=80\">Fernleigh track</a> through <a href=\"http://www.belmontwetlands.com.au/\">Belmont Wetlands State Park</a> to Redhead.",
+		"There the walk follows the coastal track through <a href=\"http://www.environment.nsw.gov.au/NationalParks/parkHome.aspx?id=N0522\">Awabakal</a> and <a href=\"https://www.nationalparks.nsw.gov.au/visit-a-park/parks/glenrock-state-conservation-area\">Glenrock State Conservation Area</a>."
+	],
+	"duration" : 10,
+	"length" : 43,
+	"zoom" : 13,
+	"rain" : false,
+	"fireban" : false,
+	"markers" : {
+		"start" : {
+			"location" : "Teralba",
+			"type" : "train",
+			"icon" : "./inc/img/marker-train.png",
+			"description" : "Get the train to Adamstown using <a href=\"https://transportnsw.info/trip#/?to=Teralba\">transportnsw.info</a>."
+		},
+		"end" : {
+			"location" : "Newcastle",
+			"type" : "train",
+			"icon" : "./inc/img/marker-train.png",
+			"description" : "Plan your return trip from Teralba at <a href=\"https://transportnsw.info/trip#/?from=Queens Wharf\">transportnsw.info</a>.",
+			"lat" : -32.962537000000005,
+			"lon" : 151.603939
+		},
+		"kioska" : {
+			"type" : "kiosk",
+			"icon" : "./inc/img/marker-kiosk.png",
+			"lat" : -32.978277000000006,
+			"lon" : 151.708188
+		},
+		"kioskb" : {
+			"type" : "kiosk",
+			"icon" : "./inc/img/marker-kiosk.png",
+			"lat" : -33.036977,
+			"lon" : 151.659464
+		},
+		"kioskc" : {
+			"type" : "kiosk",
+			"icon" : "./inc/img/marker-kiosk.png",
+			"lat" : -32.97394722222222,
+			"lon" : 151.64323888888887
+		},
+		"kioskd" : {
+			"type" : "kiosk",
+			"icon" : "./inc/img/marker-kiosk.png",
+			"lat" : -33.009511,
+			"lon" : 151.635204
+		},
+		"toileta" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -32.977214000000004,
+			"lon" : 151.708441
+		},
+		"toiletb" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -32.974841,
+			"lon" : 151.644288
+		},
+		"toiletc" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -33.035152777777775,
+			"lon" : 151.6571138888889
+		},
+		"toiletd" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -33.011038,
+			"lon" : 151.628616
+		},
+		"toilete" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -33.028398,
+			"lon" : 151.635704
+		},
+		"toiletf" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -33.009252,
+			"lon" : 151.634937
+		},
+		"toiletg" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -33.000419,
+			"lon" : 151.640301
+		},
+		"toileth" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -32.961178,
+			"lon" : 151.614977
+		},
+		"toileti" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -32.988935,
+			"lon" : 151.636190
+		},
+		"toiletj" : {
+			"type" : "toilet",
+			"icon" : "./inc/img/marker-toilet.png",
+			"lat" : -32.963656,
+			"lon" : 151.604771
+		}
+	},
+	"indicator" : {
+		"icon" : "./inc/img/marker-photo.png",
+		"description" : "Photo taken at this location."
+	},
+	"landmarks" : {
+		"dsc_3128" : ""
+	}
+}
