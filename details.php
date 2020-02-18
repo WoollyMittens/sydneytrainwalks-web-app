0357afa680b1fd6eb71c8b66b44f016585e8108d
Maurice van Creij <maurice.vancreij@webqem.com>
2020-02-19 10:03:29 +1100

    Tweak titles for SEO

--- details.php
+++ details.php
@@ -21,7 +21,7 @@
 		if (property_exists($json, 'alias')) { $assets = $json->{'alias'}->{'key'}; }
 
 		// summary
-		$subtitle = "From " . $firstMarker->{'location'} . " to " . $lastMarker->{'location'} . " via " . $json->{'location'};
+		$subtitle = "A hiking trip from " . $firstMarker->{'location'} . " to " . $lastMarker->{'location'} . " via " . $json->{'location'};
 		$description = join(' ', $json->{'description'});
 		$description = strip_tags($description);
 		$displayDate = date("d M Y", strtotime($json->{'updated'}));
@@ -32,11 +32,11 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta http-equiv="imagetoolbar" content="no"/>
 		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<title><?php print $title ?> - <?php echo $subtitle?></title>
+		<title><?php echo $subtitle?> - <?php print $title ?></title>
 		<meta name="viewport" content="initial-scale=1, minimum-scale=1, maximum-scale=1, width=device-width, user-scalable=yes"/>
 		<meta property="og:url" content="https://<?php print $domain ?>/details.php?id=<?php echo $id ?>" />
 		<meta property="og:image" content="https://<?php print $domain ?>/inc/social/<?php echo $id ?>.png" />
-		<meta property="og:title" content="<?php print $title ?> - <?php echo $subtitle ?>" />
+		<meta property="og:title" content="<?php echo $subtitle ?> - <?php print $title ?>" />
 		<meta property="og:description" content="<?php echo $description ?>" />
 		<meta name="msapplication-TileColor" content="#558b2f" />
 		<meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
